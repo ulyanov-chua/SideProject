0da6a3c0c07aeaf15ff404e3d4b0fd16ac2ba8d1
taiwansmurf2 <karllim31@yahoo.com>
2018-04-23 15:05:52 +0800

    added project files

---
+++ Character.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gladiatorGame
+{
+    class Character
+    {
+
+        string name;
+        int strength;
+        int agility;
+        int intelligence;
+        int luck;
+        int vitality;
+
+        public Character()
+        {
+            strength = 3;
+            agility = 3;
+            intelligence = 3;
+            luck = 3;
+            vitality = 3;
+        }
+
+        public void showCharInfo()
+        {
+            Console.WriteLine("Name : {0}", name);
+            Console.WriteLine("strength : {0}\nagility : {1}\nintelligence : {2}\nluck : {3}\nvitality : {4}\n", strength, agility, intelligence, luck, vitality);
+        }
+
+        public void basicAttack(Character target)
+        {
+
+        }
+
+        public void setName(string newName)
+        {
+            name = newName;
+        }
+
+       
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gladiatorGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter character name :");
+            Character player = new Character();
+            player.setName(Console.ReadLine());
+            player.showCharInfo();
+            Console.ReadKey();
+        }
+    }
+}
